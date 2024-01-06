01aa6788a7ce0ddbf5c01874056da93281905382
ImSrishti <shristi998@gmail.com>
2024-01-06 11:02:40 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Spacex from './components/spacex';
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <Spacex />
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+//starting api integration
